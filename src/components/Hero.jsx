930752c56ed9c6b12c3e2a388ca836ad6126cfe6
Rahul Arora <rahularora2715@gmail.com>
2024-01-05 23:23:24 +0530

    did stuff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="h-screen w-full flex items-center">
       <div className="ml-56 text-custom-icon-hover">
-        <h1 className="font-Madurai md:text-4xl lg:text-6xl xl:text-8xl">Hey! I am Rahul.</h1>
+        <h1 className="font-Madurai text-6xl">Hey! I am Rahul.</h1>
         <h2 className="font-Madurai text-4xl mt-2">&lt;Software Developer /&gt;</h2>
       </div>
     </div>
