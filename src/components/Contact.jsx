40d5b117832cf47942af869e81435ac58a7f62b2
Rahul Arora <rahularora2715@gmail.com>
2024-02-22 00:08:25 +0530

    stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     <div className="min-h-screen flex justify-center items-center text-custom-icon-hover">
       <div className="text-center max-w-2xl">
         <p className="font-Madurai text-5xl">Get in Touch</p>
-        <p className="text-lg mt-4">
+        <p className="text-lg mt-4 mb-10">
           I'm actively seeking new opportunities, and my inbox is wide open! Whether you have a question or an exciting
           opportunity to share, I'll be thrilled to hear from you and will make every effort to respond promptly. Let's
           connect and explore the possibilities together!
@@ -22,7 +22,7 @@ const Contact = () => {
           className="text-custom-icon-hover font-bold text-xl"
           onClick={toggleTextVisibility}
         >
-          {isTextVisible ? 'rahularora2715@gmail.com' : <IoIosMail size={30} />}
+          {isTextVisible ? 'rahularora2715@gmail.com' : <IoIosMail size={22}/>}
         </button>
       </div>
     </div>
