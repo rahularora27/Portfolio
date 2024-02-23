d3a19206b62f9d003ef4f6361eaa3ffc494e1af5
Rahul Arora <rahularora2715@gmail.com>
2024-02-23 20:31:03 +0530

    did stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,19 +10,17 @@ const Contact = () => {
 
   return (
     <div className="min-h-screen flex justify-center items-center text-custom-icon-hover">
-      <div className="text-center max-w-2xl">
+      <div className="text-center max-w-md sm:max-w-3xl px-4 sm:px-8">
         <p className="font-Madurai text-5xl">Get in Touch</p>
-        <p className="text-lg mt-4 mb-10">
-          I'm actively seeking new opportunities, and my inbox is wide open! Whether you have a question or an exciting
-          opportunity to share, I'll be thrilled to hear from you and will make every effort to respond promptly. Let's
-          connect and explore the possibilities together!
+        <p className="text-lg mt-4 mb-10 sm:text-xl">
+          I'm actively seeking new opportunities, and my inbox is wide open! Whether you have a question or an exciting opportunity to share, I'll be thrilled to hear from you and will make every effort to respond promptly. Let's connect and explore the possibilities together!
         </p>
 
         <button
           className="text-custom-icon-hover font-bold text-xl"
           onClick={toggleTextVisibility}
         >
-          {isTextVisible ? 'rahularora2715@gmail.com' : <IoIosMail size={22}/>}
+          {isTextVisible ? 'rahularora2715@gmail.com' : <IoIosMail size={22} />}
         </button>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <div className="text-center px-4 max-w-3xl mx-auto">
-        <h1 className="font-Madurai text-6xl text-custom-icon-hover">Hey! I am Rahul.</h1>
-        <h2 className="font-Madurai text-4xl mt-2 text-custom-icon-hover">&lt;Software Developer /&gt;</h2>
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-center max-w-3xl">
+        <h1 className="font-Madurai text-3xl md:text-5xl lg:text-7xl text-custom-icon-hover">Hey! I am Rahul.</h1>
+        <h2 className="font-Madurai text-xl md:text-3xl lg:text-5xl text-custom-icon-hover">&lt;Software Developer /&gt;</h2>
       </div>
     </div>
   );
