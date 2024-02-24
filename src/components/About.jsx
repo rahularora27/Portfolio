ec87fe80588ae7d5ac7df7cf72201b718bb112f7
Rahul Arora <rahularora2715@gmail.com>
2024-02-24 19:08:40 +0530

    did stuff

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='h-screen w-full text-white'>About</div>
+    <div className='h-screen flex items-center justify-around'>
+      <div>
+        Photo
+      </div>
+      <div className='text-custom-icon-hover max-w-3xl'>
+        Hi there! <br/>
+        I'm Rahul Arora, a 3rd-year BTech CSE student at Manipal University Jaipur, passionate about building innovative and user-friendly web applications. I'm constantly learning and exploring new technologies, and I'm excited to share my journey and creations with you.
+        <br/>
+        Technologies I'm familiar with: <br/>
+        JavaScript
+        React
+        MongoDB
+        MySQL
+        Node.js
+        Express.js
+        <br/>
+        This portfolio showcases some of my projects and experiences. I'd love to hear your thoughts, so feel free to browse around and connect with me!
+        <br/>
+        Beyond the code:
+        <br/>
+        When I'm not coding, you might find me:
+        Mastering a new game (currently obsessed with [insert your favorite game]).
+        Catching up with friends and family.
+        Playing Cricket. (Love it)
+        <br/>
+        Ready to collaborate?
+        I'm always looking for opportunities to learn and grow. If you have a project in mind or simply want to chat about tech, don't hesitate to reach out!
+      </div>
+    </div>
   )
 }
 
