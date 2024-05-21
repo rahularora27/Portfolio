34b7b5145ea7be885a259e9c7d142a8a6e831c50
Rahul Arora <rahularora2715@gmail.com>
2024-05-21 08:15:55 +0530

    fixed navbar

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,62 +1,152 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { Transition } from '@headlessui/react';
 import { Link } from 'react-scroll';
-import { BiMenuAltRight } from "react-icons/bi";
 
-const NAV_LINKS = [
-  { to: 'home', label: 'Home' },
-  { to: 'about', label: 'About' },
-  { to: 'projects', label: 'Projects' },
-  { to: 'contact', label: 'Contact?' },
-];
-
-const TopNav = () => {
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
   };
 
   const commonClassName = 'text-lg font-Arvo font-semibold cursor-pointer text-custom-1 hover:text-custom-2 hover:scale-110 transition duration-500';
+  const commonClassName2 = 'block text-lg font-Arvo font-semibold cursor-pointer text-custom-1';
 
   return (
-    <header className="flex flex-wrap md:justify-start md:flex-nowrap w-full text-md py-4 relative">
-      <nav className='max-w-[85rem] w-full mx-auto flex flex-wrap basis-full items-center justify-end' role="navigation">
-        <div className="md:hidden md:order-3 flex items-center gap-x-2">
-          <button
-            type="button"
-            className="p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg text-custom-1 hover:text-custom-icon-2 shadow-md"
-            onClick={toggleMenu}
-            aria-label="Toggle navigation"
-          >
-            <BiMenuAltRight
-              className={`hs-collapse-open:${isMenuOpen ? 'block' : 'hidden'} flex-shrink-0 size-4`}
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </button>
-        </div>
-        <div
-          id="navbar-alignment"
-          className={`hs-collapse ${isMenuOpen ? 'block' : 'hidden'} overflow-hidden basis-full grow md:grow-0 md:basis-auto md:block md:order-2`}
-        >
-          <div className="md:mt-0 md:ps-5 mx-2">
-            <ul className='md:flex items-center fixed right-4 md:space-x-4 font-Arvo'>
-              {NAV_LINKS.map((link) => (
-                <li key={link.to} className={commonClassName}>
-                  <Link to={link.to} spy={true} smooth={true} duration={500} offset={-30}>
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+    <nav className="fixed top-0 w-full bg-gray-800 z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            {/* Logo */}
+            <div className="text-white text-2xl"></div>
+          </div>
+          <div className="hidden md:block">
+           <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName}
+              >
+                Home
+              </Link>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName}
+              >
+                About
+              </Link>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName}
+              >
+                Projects
+              </Link>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName}
+              >
+                Contact?
+              </Link>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button onClick={toggleMenu} className="text-custom-1 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+              <BiMenuAltRight className="block h-6 w-6" />
+            </button>
           </div>
         </div>
-      </nav>
-    </header>
+      </div>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        {(ref) => (
+          <div className="md:hidden" id="mobile-menu">
+            <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName2}
+                onClick={handleLinkClick}
+              >
+                Home
+              </Link>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName2}
+                onClick={handleLinkClick}
+              >
+                About
+              </Link>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName2}
+                onClick={handleLinkClick}
+              >
+                Projects
+              </Link>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-30}
+                duration={500}
+                className={commonClassName2}
+                onClick={handleLinkClick}
+              >
+                Contact?
+              </Link>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </nav>
   );
 };
 
-export default TopNav;
+export default Navbar;
